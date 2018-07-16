6ab28a1bb5bc02f5d3270c3379d42c997722efd7
muskan195 <muskanrastogi195@gmail.com>
2018-07-16 10:38:46 +0530

    Main Footer Updated

--- resources/views/main_footer.php
+++ resources/views/main_footer.php
@@ -1,4 +1,11 @@
- <footer class="py-5 bg-dark">
+<style>
+.footer-custom {
+    
+    background-color:#990000;
+}
+</style>
+<footer class="py-5 footer-custom">
+ <!--<footer class="py-5 bg-custom" >-->
       <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; Blood Bank System 2018</p>
       </div>
@@ -6,3 +13,8 @@
     </footer>
  <script src="vendor/jquery/jquery.min.js"></script>
  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+ 
+ 
+ 
+ 
+ <!--<nav class="navbar navbar-inverse navbar-fixed-top navbar-custom">-->
