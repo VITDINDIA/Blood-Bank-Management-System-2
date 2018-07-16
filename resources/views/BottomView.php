9b104dab11f32e3c6669aa5ff9eebf9777eb19ab
muskan195 <muskanrastogi195@gmail.com>
2018-07-16 21:59:25 +0530

    BottomView updated

--- resources/views/BottomView.php
+++ resources/views/BottomView.php
@@ -1,25 +1,37 @@
  <section class="py-5">
-      <div class="container" >
-        <hr />
+  <div class="container" >
+   <hr/>
 <div class="row">
   <div class="col-sm-4">
+ <div class="well">  
+<bold> <h3>Services</h3></bold>
+  <div class="row">
+    <?php
+  $res=$database->get_all_services();
+  while($fetch=mysqli_fetch_array($res))
+      {
+	  $name=$fetch[1]." ";
+      ?>
+      <p><?php  echo"$fetch[1]";  ?></p>
+		
+  <?php
+       } 
+  ?>   
+  </div>
+  </div>
+  </div> 
+  
+  
+  <div class="col-sm-4">
  <div class="well">
- <h3>Services</h3>
- <p >1. Search Hospitals for Blood Banks. </p>
- <p >2. Find Out The Availability of Blood.   </p>
- <p >3. Generate Request for The Blood.  </p>
- <p >4. Navigate Hospitals with The Integrated Google Maps.  </p>
- </div>
- </div>
- <div class="col-sm-4">
- <div class="well">
-  <h3>Service Location</h3>
+ <strong> <h3>Service Location</h3></strong>
    <?php     $Db_objects->cities();   ?>
- </div>
- </div>
+   </div>
+   </div>
+ 
  <div class="col-sm-4">
  <div class="well">
-  <h3>Contact Information</h3>
+ <strong> <h3>Contact Information</h3></strong>
    <p ></p>
    <p >E-mail: abhinav.cse12@gmail.com</p>
  </div>
